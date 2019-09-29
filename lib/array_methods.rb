4ba455dea7cf0f661147fc83ae95d848eb364a94
Jasmine Kim <jas89922@gmail.com>
2019-09-29 23:03:51 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,15 +3,15 @@ array.include?(element)
 end
 
 def using_sort(array)
-
+array.sort(element)
 end
 
 def using_reverse(array)
-
+array.reverse(element)
 end
 
 def using_first(array)
-
+array.first(element)
 end
 
 def using_last(array)
